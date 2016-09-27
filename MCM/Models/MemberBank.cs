8b26a65a50f2116a0632293a0476cbd96c26dd71
tagamaga <taga.maga@mymtnmail.co.za>
2016-09-27 23:31:40 +0200

    Adjust column name

--- MCM/Models/MemberBank.cs
+++ MCM/Models/MemberBank.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace MCM.Models
 {
     public class MemberBank
     {
-        public int mBankID { get; set; }
+        public int MemberBankID { get; set; }
         public int MemberID { get; set; }
         public int BankID { get; set; }
         public int BranchID { get; set; }
