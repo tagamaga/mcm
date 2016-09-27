c91952bd256c835b505aaee6fc28388792ff62bb
tagamaga <taga.maga@mymtnmail.co.za>
2016-09-27 23:29:34 +0200

    Remove Area references as per Client request

--- MCM/Models/MemberLocation.cs
+++ MCM/Models/MemberLocation.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace MCM.Models
 {
@@ -9,11 +7,9 @@ namespace MCM.Models
     {
         public int mLocationID { get; set; }
         public int MemberID { get; set; }
-        public int? AreaID { get; set; }
         public int RegionID { get; set; }
 
         public virtual ICollection<Member> Members { get; set; }
-        public virtual ICollection<Area> Areas { get; set; }
         public virtual ICollection<Region> Regions { get; set; }
     }
 }
