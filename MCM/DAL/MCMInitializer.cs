e7a830643e707cadd1aeaf84ef291fac4886928d
tagamaga <taga.maga@mymtnmail.co.za>
2016-09-27 20:59:51 +0200

    Added Initializer + XLS file

---
+++ MCM/DAL/MCMInitializer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using MCM.Models;
+
+namespace MCM.DAL
+{
+    public class MCMInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<MCMContext>
+    {
+        protected override void Seed(MCMContext context)
+        {
+            base.Seed(context);
+        }
+    }
+}
