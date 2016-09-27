27ba8652ddb068ed7bc4e0c1ef43a3920ebeb816
tagamaga <taga.maga@mymtnmail.co.za>
2016-09-27 21:14:56 +0200

    Add initializer + XLS file

--- MCM/Models/Area.cs
+++ MCM/Models/Area.cs
@@ -11,7 +11,7 @@ namespace MCM.Models
         public int RegionID { get; set; }
         public string AreaCode { get; set; }
         public string AreaName { get; set; }
-        public int pCodeID { get; set; }
+        public int PostalCodeID { get; set; }
         public bool AreaActive { get; set; }
         public virtual ICollection<Region> Regions { get; set; }
         public virtual ICollection<PostalCode> PostalCodes { get; set; }
